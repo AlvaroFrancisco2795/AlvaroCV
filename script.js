2885bcbaade1277a0861b7d37b3474fd93de758e
AlvaroFrancisco2795 <soyalvaromoreno@hotmail.com>
2023-01-30 19:39:21 -0300

    all page

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuVisible = false;
+//Funcion que oculta o muestra el menu
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar() {
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+
+}
+
+//Funcion que aplica animaciones de las habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("sql");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("dedicacion");
+        habilidades[7].classList.add("liderazgo");
+    }
+
+}
+
+//Detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function () {
+    efectoHabilidades();
+}
